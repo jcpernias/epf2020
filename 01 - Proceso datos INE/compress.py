a6c957e158127c87cb26c7221af04659d40452f5
José C. Pernías <jcpernias@gmail.com>
2021-11-23 19:36:53 +0100

    Add data for 2019

--- 01 - Proceso datos INE/compress.py
+++ 01 - Proceso datos INE/compress.py
@@ -1,9 +1,13 @@
 import zipfile
 import bz2
+import os.path
 
 def unzip(fname, path = None):
   with zipfile.ZipFile(fname, "r") as zfile:
-    zfile.extractall(path)
+    members = [ x for x in zfile.infolist() if not x.is_dir() ]
+    for m in members:
+      m.filename = os.path.basename(m.filename) # remove directories
+      zfile.extract(m, path)
 
 def compress(ipath, opath, chunk_size = 1024):
   compressor = bz2.BZ2Compressor()
